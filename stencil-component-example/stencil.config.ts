937abe9f9d7624bc3ef98b364b2b974a06fcc62b
Richard Shephard <richard.shephard@gmail.com>
2021-12-21 17:14:39 +0200

    update baseurl

--- stencil-component-example/stencil.config.ts
+++ stencil-component-example/stencil.config.ts
@@ -16,6 +16,7 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
+      baseUrl: 'https://poimen.github.io/stencil-tailwind-plugin-example/'
     },
   ],
   plugins: [
