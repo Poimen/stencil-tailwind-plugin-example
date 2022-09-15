34ee4a5a0f5770bb046a6b77f0e7d4d04db6f90d
Richard Shephard <richard.shephard@gmail.com>
2022-09-15 12:21:43 +0200

    update versions

--- stencil-component-example/stencil.config.ts
+++ stencil-component-example/stencil.config.ts
@@ -2,9 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
 import tailwindcss from 'tailwindcss';
-import { defaultExtractor } from 'tailwindcss/lib/lib/defaultExtractor';
 import tailwindConf from './tailwind.config';
-import purgecss from '@fullhuman/postcss-purgecss';
 import autoprefixer from 'autoprefixer';
 
 export const config: Config = {
@@ -32,22 +30,10 @@ export const config: Config = {
       postcss: {
         plugins: [
           tailwindcss(),
-          purgecss({
-            content: ['./**/*.tsx'],
-            safelist: [
-              ':root',
-              ':host',
-              ':shadow',
-              '/deep/',
-              '::part',
-              '::theme'
-            ],
-            defaultExtractor
-          }),
-          autoprefixer(),
+          autoprefixer()
         ]
       }
     }),
-    tailwindHMR(),
+    tailwindHMR()
   ]
 };
