c859b71a22f3bc3209758e5fc550f7c3271f140d
Richard Shephard <richard.shephard@gmail.com>
2022-11-25 11:36:40 +0200

    update dependencies

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,20 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
+import tailwind, { tailwindHMR, setPluginConfigurationDefaults } from 'stencil-tailwind-plugin';
 import tailwindcss from 'tailwindcss';
 import tailwindConf from './tailwind.config';
 import autoprefixer from 'autoprefixer';
+
+setPluginConfigurationDefaults({
+  tailwindConf,
+  tailwindCssPath: './src/styles/tailwind.css',
+  postcss: {
+    plugins: [
+      tailwindcss(),
+      autoprefixer()
+    ]
+  }
+});
 
 export const config: Config = {
   namespace: 'stencil-component-example',
@@ -24,16 +35,7 @@ export const config: Config = {
   ],
   plugins: [
     sass(),
-    tailwind({
-      tailwindConf,
-      tailwindCssPath: './src/styles/tailwind.css',
-      postcss: {
-        plugins: [
-          tailwindcss(),
-          autoprefixer()
-        ]
-      }
-    }),
+    tailwind(),
     tailwindHMR()
   ]
 };
